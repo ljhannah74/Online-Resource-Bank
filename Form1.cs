c52f29fe163262f25a471a214681f61f81e57413
Lewis Hannah <lewis.j.hannah@gmail.com>
2025-02-28 16:15:41 -0500

    Upgrade Nuget Packages

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.Common;
 using System.Data.OleDb;
+// TODO Although ClickOnce is supported on .NET 5+, apps do not have access to the System.Deployment.Application namespace. For more details see https://github.com/dotnet/deployment-tools/issues/27 and https://github.com/dotnet/deployment-tools/issues/53.
 using System.Deployment.Application;
 using System.Diagnostics;
 using System.Drawing;
